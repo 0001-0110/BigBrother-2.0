60131af03c841b6fd32d8e01f476cd62d94c1d05
Rémi TROTEL <remi.trotel@epita.fr>
2022-12-03 00:03:47 +0100

    renamed var

--- Program.cs
+++ Program.cs
@@ -2,10 +2,10 @@
 
 public static class Program
 {
-    private static string tokenFile = "C:\\Users\\remi\\OneDrive\\Documents\\travail\\Prog\\C#\\BigBrother\\BigBrother\\Data";
+    private static string dataFolder = "C:\\Users\\remi\\OneDrive\\Documents\\travail\\Prog\\C#\\BigBrother\\BigBrother\\Data";
 
     public static async Task Main(string[] args)
     {
-        await new BigBrother(tokenFile).Run();
+        await new BigBrother(dataFolder).Run();
     }
 }
