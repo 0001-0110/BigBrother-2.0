7f24df3700f7f95d242259402a5b8dd992955eb5
Rémi TROTEL <remi.trotel@epita.fr>
2022-12-08 21:19:55 +0100

    fixed a bug in the say command

--- Say.cs
+++ Say.cs
@@ -11,15 +11,17 @@ internal partial class BigBrother
     private async Task Say(IMessage message, GroupCollection args)
     {
         ulong channelId;
-        if (ulong.TryParse(args[1].Value, out channelId))
+        if (!ulong.TryParse(args[1].Value, out channelId))
         {
             await SendMessage(message.Channel, "Incorrect channel ID");
+            return;
         }
 
         IMessageChannel? channel = client.GetChannel(channelId) as IMessageChannel;
         if (channel == null)
         {
             await SendMessage(message.Channel, "Incorrect channel ID");
+            return;
         }
 
         await SendMessage(channelId, args[2].Value);
