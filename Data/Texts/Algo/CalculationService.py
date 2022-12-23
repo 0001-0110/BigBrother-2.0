7fb9fa41d2d28e5ac238277693dda04c4013d8e3
Rémi TROTEL <remi.trotel@epita.fr>
2022-12-24 00:08:28 +0100

    removed all texts, as this data is no longer necessary

--- Data/Texts/Algo/CalculationService.py
+++
@@ -1,5 +0,0 @@
-
-
-#calcul la distance (au carré) entre positionA et positionB
-def distanceSquare(positionA,positionB):
-    return (positionA.x - positionB.x)**2 + (positionA.y - positionB.y)**2
--- Data/Texts/Algo/Exit.py
+++
@@ -1,7 +0,0 @@
-class Exit:
-
-    def __init__(self,position):
-        self.position = position
-        #self.state = "closed"
-        #load sprite
-        #self.sprite = pygame.image.load("a_HMI/assets/sprites/Back_001.png")
